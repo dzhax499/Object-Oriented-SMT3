81541632615d1abb853a8e10854b28c05bbd842b
dzhax499 <dzdzakir@gmail.com>
2025-08-28 18:44:04 +0700

    looping menu

--- W2-PRAKTIKUM/koperasi.java
+++ W2-PRAKTIKUM/koperasi.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class koperasi{
-    public 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Produk[] daftarProduk = {
+            new Produk("Pulpen", 3500,10, 0),
+            new Produk("Buku",7000,15,0),
+            new Produk("Pensil",300,7,0)
+        };
+        
+        System.out.println("-- MENU PRODUK KOEPARSI --");
+        for (int i = 0 ;i < daftarProduk.length; i++) {
+            System.out.println((i+1) + ". " + daftarProduk[i].Getnama_prod() + " Rp. " + daftarProduk[i].Getharga_prod()  );
+        }
+
+        System.out.print("Pilih barang yang mau dibeli (1 - " + daftarProduk.length + "): " );
+        int pilih = input.nextInt();
+    }
 }
--- W2-PRAKTIKUM/produk.java
+++ W2-PRAKTIKUM/produk.java
@@ -1,11 +1,11 @@
-public class produk {
+public class Produk {
  private String nama_prod;
  private int harga_prod;
  private int stok_prod;
  private int jumlah_prod;
 
- public produk{
-    this.nama_prod = nama_produk;
+ public Produk(String nama_prod, int harga_prod,int stok_prod,int jumlah_prod) {
+    this.nama_prod = nama_prod;
     this.harga_prod = harga_prod;
     this.stok_prod = stok_prod;
     this.jumlah_prod = jumlah_prod;
@@ -14,7 +14,7 @@ public class produk {
     return nama_prod;
  }
  public int Getharga_prod(){
-    return harga_prod
+    return harga_prod;
  }
  public int Getstok_prod(){
     return stok_prod;
