0436d995404619a7d54b6696f4aa8a92fc3c637d
dzhax499 <dzdzakir@gmail.com>
2025-08-28 16:15:14 +0700

    menambahkan getter produk, dan mengengkapsulasi

---
+++ W2-PRAKTIKUM/produk.java
@@ -0,0 +1,26 @@
+public class produk {
+ private String nama_prod;
+ private int harga_prod;
+ private int stok_prod;
+ private int jumlah_prod;
+
+ public produk{
+    this.nama_prod = nama_produk;
+    this.harga_prod = harga_prod;
+    this.stok_prod = stok_prod;
+    this.jumlah_prod = jumlah_prod;
+ }
+ public String Getnama_prod(){
+    return nama_prod;
+ }
+ public int Getharga_prod(){
+    return harga_prod
+ }
+ public int Getstok_prod(){
+    return stok_prod;
+ }
+ public int Getjumlah_prod(){
+    return jumlah_prod;
+ }
+
+}
