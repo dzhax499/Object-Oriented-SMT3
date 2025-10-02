c9476dcc626be37560c115860e0c0cece450ef0d
dzhax499 <dzdzakir@gmail.com>
2025-10-02 13:04:41 +0700

    12.4

---
+++ TEORI/Tugas_7_Exception/12.4-Finally/Finally.java
@@ -0,0 +1,30 @@
+class Finally
+{
+    private static int j=0;
+
+    public static void main(String[] args)
+    {
+        while(true)
+        {
+            try
+            {
+                System.out.print("pada saat i = " + j + ": ");
+                if(j++ == 0)
+                    throw new Exception();
+                System.out.println("tidak terjadi eksepsi");
+            }
+            catch(Exception x)
+            {
+                System.out.println("terdapat eksepsi");
+            }
+            finally
+            {
+                System.out.println("statemen dalam blok finally");
+                if(j == 3)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
